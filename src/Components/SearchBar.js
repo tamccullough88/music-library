d765533953231cd41e32584af63e1a03a0c5f889
tamccullough88 <tamccullough88@gmail.com>
2023-11-27 19:07:38 -0500

    updating search bar to fix search clearing on submit

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -5,8 +5,8 @@ function SearchBar({ handleSearch }) {
     const [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <form>
-            <input onChange={(e) => handleSearch(e, e.target.value)} placeholder="Search" />
+        <form onSubmit={(e) => handleSearch(e, searchTerm)}>
+            <input onChange={(e) => handleSearch(e, e.target.value)} onChangeCapture={(e) => setSearchTerm(e.target.value)} placeholder="Search" />
             <input type='submit' value="Search" />
         </form>
     )
