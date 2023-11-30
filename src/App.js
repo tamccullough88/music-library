c2f4ce17367409b52dfcb0dc2a138a35a5e85546
tamccullough88 <tamccullough88@gmail.com>
2023-11-29 21:00:58 -0500

    adding message to index page

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ function App() {
           <Route path='/' element={
             <>
               <SearchBar handleSearch={handleSearch} />
+              {message}
               <Gallery data={data} />
             </>
           } />
